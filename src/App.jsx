c65e4f530d4e6bcc56791b68cfa607cfffe46c58
DiegoGugo <diego.gugo@outlook.com>
2022-01-21 20:29:20 -0600

    inisialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import react, {Fragment} from 'react'
+
+const App = () => {
+    return(
+        <Fragment>
+            <h1>Buenas noches💩</h1>
+        </Fragment>
+    )
+}
+
+export default App
